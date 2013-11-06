b0664d67405014d527fc352e1065f6367ae5d07a
nikhilkapoor <nikhilkapoor01@gmail.com>
2013-11-06 17:51:23 -0500

    Create MeetingTimes.java

---
+++ MeetingTimes.java
@@ -0,0 +1,22 @@
+package csce747.nikhil.takehome1;
+
+/**
+ * @author nikhil
+ *
+ */
+public class MeetingTimes {
+
+	Time mt1 = new Time();
+	Time mt2 = new Time();
+
+	Time mt3 = new Time("testing",1005,75);
+	Time mt4 = new Time("info_ret",3715,75);
+	Time mt5 = new Time("algo",3850,75);
+	Time mt6 = new Time("102Lect",4130,50);
+
+	Time mt7 = new Time("testing",5805,75);
+	Time mt8 = new Time("info_ret",8515,75);
+	Time mt9 = new Time("algo",8650,75);
+	Time mt10 = new Time("102Lect",8930,50);
+
+}
